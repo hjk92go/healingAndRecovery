e736cb9df1051c656d86df3775a5dd3c9c2c6d8b
Hyeonju Kim <hjk92go@gmail.com>
2023-04-16 14:36:04 +0900

    writeList

--- src/components/WriteList.jsx
+++ src/components/WriteList.jsx
@@ -1,10 +1,9 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
 import list from "../css/WriteList.module.css";
-
+import { useMediaQuery } from "@material-ui/core";
 const style = {
   position: "absolute",
   top: "50%",
@@ -19,12 +18,26 @@ const style = {
   color: "black",
   overflowY: "scroll",
 };
+const useStyle = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  height: "400px",
+  width: "300px",
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+  color: "black",
+  overflowY: "scroll",
+};
 
 export default function BasicModal({ toss }) {
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
-
+  const isMobile = useMediaQuery("(max-width: 420px)");
   return (
     <div className={list.aa}>
       <Button onClick={handleOpen}>지금까지 보관된 일상들</Button>
@@ -32,15 +45,17 @@ export default function BasicModal({ toss }) {
         open={open}
         onClose={handleClose}
         aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description">
+        aria-describedby="modal-modal-description"
+      >
         <div className={list.box}>
-          <Box sx={style}>
+          <Box sx={isMobile ? useStyle : style}>
             <table>
               <div
                 id="modal-modal-title"
                 variant="h6"
                 component="thead"
-                className={list.TbTitle}>
+                className={list.TbTitle}
+              >
                 <tr>
                   <th>나의 하루들</th>
                 </tr>
@@ -49,7 +64,8 @@ export default function BasicModal({ toss }) {
                 className={list.listTb}
                 id="modal-modal-description"
                 sx={{ mt: 2 }}
-                component="tbody">
+                component="tbody"
+              >
                 {toss}
               </div>
             </table>
